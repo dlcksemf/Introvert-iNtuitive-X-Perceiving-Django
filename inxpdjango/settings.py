c1eb4d73b792b358b132c10803224b5d298a715c
Chrissy Lee <22chandl@gmail.com>
2022-02-04 11:02:54 +0900

    django mariadb 설정

--- inxpdjango/settings.py
+++ inxpdjango/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'inxpdjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql', # mysqlclient librarly 설치
+        'NAME': 'INXPdb',
+        'USER': 'root',
+        'PASSWORD': '1234', # mariaDB 설치 시 입력한 root 비밀번호 입력
+        'HOST': 'localhost',
+        'PORT': ''
     }
 }
 
