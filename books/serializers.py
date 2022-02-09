5ef020a6d1750e94e15372065b3f737a1b5ff40f
Jina Kim <fghjiurt@naver.com>
2022-02-09 13:28:31 +0900

    books category 모델 생성 및 시리얼라이저 수정, migrations 정리

--- books/serializers.py
+++ books/serializers.py
@@ -7,7 +7,27 @@ class BooksSerializer(serializers.ModelSerializer):
         model=Books
         fields=["cover_photo", "title", "writer",
     "translator", "publisher", "published_date",
-    "ISBN", "story", "state", "book_num"]
+    "ISBN", "story", "state", "book_num", "category_id"]
+
+
+    def create(self, validated_data):
+        title = validated_data["title"]
+        writer = validated_data["writer"]
+        publisher = validated_data["publisher"]
+        ISBN = validated_data["ISBN"]
+        state = validated_data["state"]
+        cover_photo = validated_data.get("cover_photo", "")
+        translator = validated_data.get("translator", "")
+        published_date = validated_data.get("published_date", "")
+        story = validated_data.get("story", "")
+        category_id = validated_data.get("category_id", "")
+
+        new_book = Books(cover_photo=cover_photo, title=title, writer=writer, publisher=publisher, ISBN=ISBN,
+                         state=state, translator=translator, published_date=published_date, story=story, category_id=category_id)
+        new_book.save()
+
+        return new_book
+
 
 class LoanedBooksSerializer(serializers.ModelSerializer):
     class Meta:
