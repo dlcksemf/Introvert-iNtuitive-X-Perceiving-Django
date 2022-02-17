e72e4ee33e0206c5c356cf8e0e815b36bd753552
Chrissy Lee <22chandl@gmail.com>
2022-02-17 14:48:50 +0900

    pagination 설정 변경 > params에 all 있으면 pagination 적용 안됨

--- books/paginations/BookApplicationsPagination.py
+++ books/paginations/BookApplicationsPagination.py
@@ -1,7 +1,24 @@
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 
 
 class BookApplicationPagination(PageNumberPagination):
     page_size = 2
     page_size_query_param = 'page_size'
-    max_page_size = 1
+
+    def get_paginated_response(self, data):
+        return Response({
+            # 'links': {
+            #     'next': self.get_next_link(),
+            #     'previous': self.get_previous_link()
+            # },
+            'count': self.page.paginator.count,
+            'total_pages': self.page.paginator.num_pages,
+            'results': data
+        })
+
+    def paginate_queryset(self, queryset, request, view=None):
+        if 'all' in request.query_params:
+            return None
+
+        return super().paginate_queryset(queryset, request, view)
