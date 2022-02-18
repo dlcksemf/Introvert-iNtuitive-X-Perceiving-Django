cbc94204dd89fee57c9f2f055dbeb6856cfc1fa5
Chrissy Lee <22chandl@gmail.com>
2022-02-18 11:32:29 +0900

    loaned books 순서대로 ordering 적용

--- books/views.py
+++ books/views.py
@@ -1,4 +1,5 @@
 from django.db.models import Q
+from rest_framework import filters
 from rest_framework.viewsets import ModelViewSet
 
 from books.models import Books, LoanedBooks, Wishes, Applications, Category
@@ -11,6 +12,8 @@ class BooksViewSet(ModelViewSet):
     queryset = Books.objects.all()
     serializer_class = BooksSerializer
     pagination_class = BookApplicationPagination
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ['loaned_books']
 
     def get_queryset(self):
         qs = super().get_queryset()
