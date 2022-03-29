b1985fef0bf73078a7644470a559236342b4876c
kangaerin <rosyty2@naver.com>
2022-03-29 10:43:42 +0900

    book serializers 임시2

--- books/serializers.py
+++ books/serializers.py
@@ -237,5 +237,5 @@ class ReviewSerializer(serializers.ModelSerializer):
 
 
 class Meta:
-    -        model = Review
-    -        fields = ["review_num", "review_content", "review_rate", "user_id", "book_name"]
+    model = Review
+    fields = ["review_num", "review_content", "review_rate", "user_id", "book_name"]
