b0766a04435ce029bc5f930c74aece932d1bb783
kangaerin <rosyty2@naver.com>
2022-04-07 10:27:23 +0900

    game amount 추가

--- game/models.py
+++ game/models.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.core.validators import MaxValueValidator
 from django.db import models
 
+
 class TimestampedModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -9,14 +10,15 @@ class TimestampedModel(models.Model):
     class Meta:
         abstract = True
 
+
 class Game(TimestampedModel):
-    game_num=models.AutoField(primary_key=True)
-    game_name=models.CharField(max_length=50, db_index=True)
-    player_num=models.CharField(max_length=10,db_index=True)
-    play_time=models.CharField(max_length=10,db_index=True)
-    level=models.CharField(max_length=10,db_index=True)
-    game_rule=models.TextField(blank=True)
-    game_state=models.CharField(
+    game_num = models.AutoField(primary_key=True)
+    game_name = models.CharField(max_length=50, db_index=True)
+    player_num = models.CharField(max_length=10,db_index=True)
+    play_time = models.CharField(max_length=10,db_index=True)
+    level = models.CharField(max_length=10,db_index=True)
+    game_rule = models.TextField(blank=True)
+    game_state = models.CharField(
         max_length=1,
         default='A',
         choices=[
@@ -25,7 +27,9 @@ class Game(TimestampedModel):
         ]
     )
 
-    game_cover_photo=models.ImageField(
+    game_amount = models.IntegerField(default=1)
+
+    game_cover_photo = models.ImageField(
         upload_to="books/%Y/%M",
         blank=True
     )
@@ -38,13 +42,13 @@ class Game(TimestampedModel):
         verbose_name="게임"
         verbose_name_plural="게임 목록"
 
-class LoanedGame(models.Model):
-    loan_game_num=models.AutoField(primary_key=True)
 
-    loaned_time=models.DateTimeField(auto_now_add=True)
-    return_due_time=models.DateTimeField()
-    returned_time=models.DateTimeField(blank=True, null=True)
-    return_state=models.CharField(
+class LoanedGame(models.Model):
+    loan_game_num = models.AutoField(primary_key=True)
+    loaned_time = models.DateTimeField(auto_now_add=True)
+    return_due_time = models.DateTimeField()
+    returned_time = models.DateTimeField(blank=True, null=True)
+    return_state = models.CharField(
         max_length=1,
         choices=[
             ("L","Loaned"),
@@ -52,25 +56,26 @@ class LoanedGame(models.Model):
         ]
     )
 
-    user_id=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
-    game_name=models.ForeignKey(Game,on_delete=models.CASCADE,related_name="loaned_game")
+    user_id = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    game_name = models.ForeignKey(Game,on_delete=models.CASCADE,related_name="loaned_game")
 
     class Meta:
-        ordering=["-loan_game_num"]
-        verbose_name="게임 대여"
-        verbose_name_plural="대여 게임 목록"
+        ordering = ["-loan_game_num"]
+        verbose_name = "게임 대여"
+        verbose_name_plural = "대여 게임 목록"
+
 
 class GameReview(TimestampedModel):
-    game_review_num=models.AutoField(primary_key=True)
-    game_review_content=models.CharField(max_length=100,db_index=True)
-    game_review_rate=models.PositiveSmallIntegerField(
+    game_review_num = models.AutoField(primary_key=True)
+    game_review_content = models.CharField(max_length=100,db_index=True)
+    game_review_rate = models.PositiveSmallIntegerField(
         validators=[
             MaxValueValidator(5),
         ]
     )
 
-    game_name=models.ForeignKey(Game,on_delete=models.CASCADE,null=True)
-    user_id=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    game_name = models.ForeignKey(Game,on_delete=models.CASCADE,null=True)
+    user_id = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
     
     class Meta:
         ordering=["-game_review_num"]
